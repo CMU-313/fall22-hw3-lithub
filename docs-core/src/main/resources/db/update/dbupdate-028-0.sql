a04aa0d2b2f9dc28782805a8e8f788330261dc69
Frederic Khayat <khayatfrederic@gmail.com>
2022-10-02 15:20:06 -0400

    Add Consistent Styling, and used foreign key to reference documents and users)

--- docs-core/src/main/resources/db/update/dbupdate-028-0.sql
+++ docs-core/src/main/resources/db/update/dbupdate-028-0.sql
@@ -1,9 +1,11 @@
-CREATE TABLE document_score_history (
-    score_change_id int  NOT NULL,
-    admin_id int  NOT NULL,
-    admin_name varchar(500)  NOT NULL,
-    document_id int  NOT NULL,
-    "user" varchar(500)  NOT NULL,
-    score int   NOT NULL,
-    CONSTRAINT document_score_history_pk PRIMARY KEY (score_change_id)
+create table T_SCORE (
+    SCORE_ID_C varchar(36) not null,
+    ADMIN_ID_C varchar(36)  not null,
+    ADMIN_USERNAME_C varchar(50) not null,
+    DOCUMENT_ID_C varchar(36) not null,
+    SCORE_I int not null,
+    SUBMITTED_B bit not null,
+    primary key (SCORE_ID_C),
+    foreign key (ADMIN_ID_C) references T_USER(USE_ID_C),
+    foreign key (DOCUMENT_ID_C) references T_DOCUMENT(DOC_ID_C)
 );
