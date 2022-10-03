3dbee84b9b3383617a706c771033c6d35a3cfe44
Frederic Khayat <khayatfrederic@gmail.com>
2022-10-02 22:53:26 -0400

    Add SetBit to database columns

--- docs-core/src/main/resources/db/update/dbupdate-028-0.sql
+++ docs-core/src/main/resources/db/update/dbupdate-028-0.sql
@@ -1,4 +1,4 @@
-create memory table T_SCORE (SCO_ID_C varchar(36) not null, SCO_IDUSER_C varchar(36) not null, SCO_IDDOC_C varchar(36) not null, SCO_USERNAME_C varchar(50) not null, SCO_VAL_C varchar(10) not null, primary key (SCO_ID_C));
+create memory table T_SCORE (SCO_ID_C varchar(36) not null, SCO_IDUSER_C varchar(36) not null, SCO_IDDOC_C varchar(36) not null, SCO_VAL_I int not null, SCO_SET_B bit not null, primary key (SCO_ID_C));
 
 alter table T_SCORE add constraint FK_SCO_IDUSER_C foreign key (SCO_IDUSER_C) references T_USER (USE_ID_C) on delete restrict on update restrict;
 alter table T_SCORE add constraint FK_SCO_IDDOC_C foreign key (SCO_IDDOC_C) references T_DOCUMENT (DOC_ID_C) on delete restrict on update restrict;
